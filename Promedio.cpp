d366b796a2ba02fe50029437b18e956a5538d1a3
MarianaLagrava <00219820@uca.edu.sv>
2020-05-12 12:02:20 -0600

    Ejercicio 1

---
+++ Promedio.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cout << "Inserte 3 numeros para determinar su promedio "<< endl;
+    cout << "Ingrese el primero "<< endl;
+    cin >> a;
+    cout << "Ingrese el segundo " << endl;
+    cin >> b;
+    cout << "Inserte el tercero " << endl;
+    cin >> c;
+    int suma = (a + b+ c) / 3; 
+    cout << "El promedio es: " << suma; 
+    return 0;
+}
+
+
+    
+    
+
